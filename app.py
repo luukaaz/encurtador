49039616a9775eb5b669b10ea5aa766a7fa7d9b6
Lucas <88948089+luukaaz@users.noreply.github.com>
2024-04-08 10:27:44 -0300

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import streamlit as st
 import pyshorteners
 
+default_value = 0
+title = default_value
 
 st.title("Encurtador de Links")
 #st.header("Qual é o seu nome")
 title = st.text_input(
-    'Digite abaixo o link',
-    'default',)
+    'Digite abaixo o seu link',
+    'link',)
 
 url = title
 link = pyshorteners.Shortener()
 shorten_url = link.tinyurl.short(url)
-st.write("Segue o seu link:", f'\n{shorten_url}')
+
+if title:
+    st.write("Segue o seu link:", f'\n{shorten_url}')
+
+title = default_value
